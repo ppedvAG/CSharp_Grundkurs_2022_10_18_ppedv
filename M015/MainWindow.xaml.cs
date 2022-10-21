e50f9b922fc35542471c4146d7652ae749e26db3
ppedvLukasK <lukask@ppedv.de>
2022-10-21 13:48:32 +0200

    M015

---
+++ M015/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace M015
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			CB.ItemsSource = new string[] { "Item1", "Item2", "Item3" };
+			LB.ItemsSource = new string[] { "Item1", "Item2", "Item3" };
+		}
+
+		private void EinButtonClicked(object sender, RoutedEventArgs e)
+		{
+			MessageBoxResult res = MessageBox.Show("Der Button wurde geklickt", "Button gedrückt", MessageBoxButton.OK, MessageBoxImage.Information);
+			if (res == MessageBoxResult.OK)
+			{
+				Test t = new Test();
+				//t.Show();
+				if (t.ShowDialog() == true)
+				{
+					Close();
+				}
+				else
+				{
+
+				}
+			}
+		}
+
+		private void CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			ComboBox cb = sender as ComboBox;
+			TB.Text = cb.SelectedValue.ToString();
+		}
+
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			TB.Text = "Checkbox checked";
+		}
+
+		private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			TB.Text = "Checkbox unchecked";
+		}
+	}
+}
---
+++ M015_Lösung/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Fahrzeugpark;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace M014_Loesung;
+
+public partial class MainWindow : Window
+{
+	public List<Fahrzeug> Fahrzeuge = new();
+
+	public MainWindow() => InitializeComponent();
+
+	private void NeuesFahrzeugClicked(object sender, RoutedEventArgs e)
+	{
+		Fahrzeuge.Add(Fahrzeug.GeneriereFahrzeug());
+		UpdateUI();
+	}
+
+	private void FahrzeugLoeschenClicked(object sender, RoutedEventArgs e)
+	{
+		if (FahrzeugLB.SelectedIndex != -1)
+		{
+			Fahrzeuge.RemoveAt(FahrzeugLB.SelectedIndex);
+			UpdateUI();
+		}
+	}
+
+	private void UpdateUI()
+	{
+		FahrzeugLB.Items.Clear();
+
+		foreach (Fahrzeug f in Fahrzeuge)
+			FahrzeugLB.Items.Add(f);
+	}
+
+	private void FahrzeugSelected(object sender, SelectionChangedEventArgs e)
+	{
+		ListBox lb = sender as ListBox;
+		InfoTB.Text = Fahrzeuge[lb.SelectedIndex].Info();
+	}
+}
